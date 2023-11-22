c7e938aba282e26d19d934777bcbe80724ae80d2
Ubuntu <ubuntu@ip-172-31-42-3.ec2.internal>
2023-11-22 07:31:45 +0000

    bootstrap導入, application.scssフォルダを作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//11/22 bootstrapを利用するために追加
+import 'bootstrap'
+import 'bootstrap/dist/js/bootstrap'
+
+import '../stylesheets/application';
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+   config.hosts << "ea418f1bcc3444b881e518b8139da290.vfs.cloud9.us-east-1.amazonaws.com"
 end
